e8507a9f62d62b2ac30ee94135f179c43bd8a938
Jevoughn Wisdom <jevoughnwisdom@gmail.com>
2020-10-31 21:01:17 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -22,8 +22,7 @@ class Song
   end
   def self.genre_count
     genre_count = Hash.new
-      @@genres.each do |genre|
-        binding.pry
+    @@genres.each do |genre|
       if genre_count[genre]
         genre_count[genre] += 1
       else
